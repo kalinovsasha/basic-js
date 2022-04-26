cbf0ecd7119700230bc42ae3759c2d2bd00a1608
Sasha_work <kalinovsasha@yandex.ru>
2022-04-26 14:26:55 +0300

    feat: add dream_team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members=[]) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(Array.isArray(members)==false){return false}
+  let buffer=[];
+    for (let key in members) {
+      if(typeof(members[key])=="string"){
+        for(let i=0;i<members[key].length;i++){
+          if(members[key][i]!=" "){
+            buffer.push(members[key][i].toUpperCase())
+            break
+          }
+        }
+      }
+    }
+  if(buffer.length<1){return false}
+  buffer = buffer.sort().join(""); 
+  return buffer
+
 }
 
+console.log(createDreamTeam([
+  ['David Abram'],
+  ['Robin Attfield'],
+  'Thomas Berry',
+  ['Paul R.Ehrlich'],
+  'donna Haraway',
+  ' BrIaN_gOodWiN  ',
+  {
+    0: 'Serenella Iovino'
+  },
+  'Erazim Kohak',
+  '  val_plumwood',
+]))
 module.exports = {
   createDreamTeam
 };
