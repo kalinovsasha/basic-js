1051cdd31e84fadfc2a0be3ee8a9561417a3c0ed
Sasha_work <kalinovsasha@yandex.ru>
2022-04-26 11:39:31 +0300

    fix: invalid date

--- src/what-season.js
+++ src/what-season.js
@@ -15,18 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSeason(date) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  //let date = new Date("03.01.2021")
-  if(date==null || date==undefined){
-    return "Unable to determine the time of year!"
+  if(date===undefined){
+    return 'Unable to determine the time of year!'
   }
+  let test
+  try {
+    test=Number(date.getMonth())
+  } catch (error) {
+    throw new Error('Invalid date!') 
+  } 
   if(date=="Invalid Date"){
     throw new Error('Invalid date!')
   }
-  if(Number(date.getMonth()==1 && date.toString().includes('Mar'))){
-    throw new Error('Invalid date!')
-  }
-  console.log(date.toISOString())
-  console.log(`date ${date.toString()} month ${Number(date.getMonth())}`)
  
   date=Number(date.getMonth())
   if(date>=0 && date<=1){
@@ -45,11 +45,10 @@ function getSeason(date) {
     return "winter"
   }
   
-  return date
 }
 
-let test=new Date(`2.30.2021`)
-console.log(getSeason(test))
+//let test=new Date(`2.30.2021`)
+//console.log(getSeason())
 
 module.exports = {
   getSeason
