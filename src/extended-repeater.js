ba2f29ac227681a2229ec00129dec2c3df1dbf95
Sasha_work <kalinovsasha@yandex.ru>
2022-04-27 16:53:35 +0300

    add: repeater init

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options={
+  repeatTimes: 1,
+  separator : "+",
+  addition :"",
+  additionRepeatTimes :1,
+  additionSeparator : "|"
+  }) 
+  {
+  let modernStr;
+  let buffer=[];
+  let separatorBuffer=[];
+  let separatorStr="";
+
+  if(options.separator==undefined){
+    options.separator="+"
+  }
+  if(options.additionSeparator==undefined){
+    options.additionSeparator="|"
+  }
+    str=str.toString();
+    try {
+      options.addition=options.addition.toString()
+    } catch (error) {
+          
+    }
+    for(let k=0;k<options.additionRepeatTimes;k++){
+      separatorBuffer.push(`${options.addition}`)
+    }
+    separatorStr=separatorBuffer.join(`${options.additionSeparator}`);
+    console.log(separatorStr)
+    for (let i=0;i<options.repeatTimes;i++){
+        buffer.push(`${str}${separatorStr}`)
+    }
+    modernStr=buffer.join(`${options.separator}`)
+  return modernStr
 }
 
+//console.log(repeater('STRING', { repeatTimes: 3, addition: '11', additionRepeatTimes: 0, additionSeparator: '--' }))
+console.log(repeater('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' }))
 module.exports = {
   repeater
 };
