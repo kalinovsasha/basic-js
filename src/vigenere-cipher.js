30b3fcdfa5f5311bc4ca25822722f919ea5539f1
Alexander <kalinovsasha@yandex.ru>
2022-04-28 23:35:38 +0300

    encrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,45 +20,105 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-    constructor(x){
+    constructor(x=true){
       this.mes;
       this.key;
       this.mesCodes=[];
       this.keyCodes=[];
-      if(x==undefined){
-        this.direction=true
-      } else this.direction= Boolean(x)
+      this.decr=[];
+      this.encr=[];
+      this.direction= Boolean(x)
     }
   encrypt(mes,key) {
+      this.mesCodes=[];
+      this.keyCodes=[];
+      this.decr=[];
+      this.encr=[];
     if(!mes || !key){
       throw new Error(`Incorrect arguments!`)
     }
     this.message=mes.toUpperCase().split(``);
     this.key=key.toUpperCase().split(``);
     let counter=0;
+    //Get ASCI - 65 codes
     for(let i in this.message){
-      this.mesCodes[i]=this.message[i].charCodeAt(0);
-      this.keyCodes[i]=this.key[counter].charCodeAt(0);
+      this.mesCodes[i]=this.message[i].charCodeAt(0)-65;
+      this.keyCodes[i]=this.key[counter].charCodeAt(0)-65;
+      //array of keys
       if(counter<this.key.length-1){
         counter++;
       } else counter=0
     }
-    console.log(this.mesCodes+`key `+ this.keyCodes)
+
+    //////////////////
+    let counter2=0;
+    for(let i=0;i< this.mesCodes.length; i++){
+      if(this.mesCodes[i]>26 ||this.mesCodes[i]<0 ){
+        this.encr[i]=String.fromCharCode(this.mesCodes[i]+65)
+      }else{
+        this.encr[i]=String.fromCharCode((this.mesCodes[i]+this.keyCodes[counter2])%26+65);
+        counter2++; //чтобы не учитывались пробелы , спецсимволы и цифры
+      }
+    }
+    console.log(this.encr.join(``))
+     if(this.direction=true){
+      return this.encr.join(``);
+     } else{
+      return this.encr.join(``).reverse();
+     }
+    
   }
   decrypt(mes,key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!mes || !key){
+      throw new Error(`Incorrect arguments!`)
+    }
+    
+    this.mesCodes=[];
+    this.keyCodes=[];
+    this.decr=[];
+    this.encr=[];
+  if(!mes || !key){
+    throw new Error(`Incorrect arguments!`)
+  }
+  this.message=mes.toUpperCase().split(``);
+  this.key=key.toUpperCase().split(``);
+  let counter=0;
+  //Get ASCI - 65 codes
+  for(let i in this.message){
+    this.mesCodes[i]=this.message[i].charCodeAt(0)-65;
+    this.keyCodes[i]=this.key[counter].charCodeAt(0)-65;
+    //array of keys
+    if(counter<this.key.length-1){
+      counter++;
+    } else counter=0
+  }
+
+  //////////////////
+  let counter2=0;
+  for(let i=0;i< this.mesCodes.length; i++){
+    if(this.mesCodes[i]>26 ||this.mesCodes[i]<0 ){
+      this.encr[i]=String.fromCharCode(this.mesCodes[i]+65)
+    }else{
+      this.encr[i]=String.fromCharCode((this.mesCodes[i]-this.keyCodes[counter2])%26-65);
+      counter2++; //чтобы не учитывались пробелы , спецсимволы и цифры
+    }
+  }
+  console.log(this.encr.join(``))
+  return this.encr.join(``);
   }
 }
 
-let test =new VigenereCipheringMachine()
+let test =new VigenereCipheringMachine(false)
 
-test.encrypt(`xyz`,`ABCDEFG`)
-console.log(String.fromCharCode(88))
+test.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey');
+test.encrypt('cryptography', 'verylongkeyword')
+//console.log(String.fromCharCode(33))
+//console.log(` `.charCodeAt(0))
 
 module.exports = {
   VigenereCipheringMachine
 };
 
-// X=65+(N-65)+(key-65)
+// X=(m+k)%26 y=(m-k)%26
+
 //A-65 Z-90
