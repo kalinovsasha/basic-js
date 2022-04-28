5617acc6ed9e8c46db02fa260317dd8d114cda2b
Sasha_work <kalinovsasha@yandex.ru>
2022-04-28 16:54:38 +0300

    init: start vinegear machine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    constructor(x){
+      this.mes;
+      this.key;
+      this.mesCodes=[];
+      this.keyCodes=[];
+      if(x==undefined){
+        this.direction=true
+      } else this.direction= Boolean(x)
+    }
+  encrypt(mes,key) {
+    if(!mes || !key){
+      throw new Error(`Incorrect arguments!`)
+    }
+    this.message=mes.toUpperCase().split(``);
+    this.key=key.toUpperCase().split(``);
+    let counter=0;
+    for(let i in this.message){
+      this.mesCodes[i]=this.message[i].charCodeAt(0);
+      this.keyCodes[i]=this.key[counter].charCodeAt(0);
+      if(counter<this.key.length-1){
+        counter++;
+      } else counter=0
+    }
+    console.log(this.mesCodes+`key `+ this.keyCodes)
   }
-  decrypt() {
+  decrypt(mes,key) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
 
+let test =new VigenereCipheringMachine()
+
+test.encrypt(`xyz`,`ABCDEFG`)
+console.log(String.fromCharCode(88))
+
 module.exports = {
   VigenereCipheringMachine
 };
+
+// X=65+(N-65)+(key-65)
+//A-65 Z-90
