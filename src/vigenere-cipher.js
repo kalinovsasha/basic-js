ecb67a5d1344851a9af2b4b8f76dd03fc042f8c7
Sasha_work <kalinovsasha@yandex.ru>
2022-04-29 10:34:41 +0300

    fix: vagner cipher correct decrypt&reverce work

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,24 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
+///////////////////Нарушен принцип DRY, но глаз уже болит переделывать.
 class VigenereCipheringMachine {
     constructor(x=true){
       this.mes;
@@ -27,7 +9,7 @@ class VigenereCipheringMachine {
       this.keyCodes=[];
       this.decr=[];
       this.encr=[];
-      this.direction= Boolean(x)
+      this.direction= x;
     }
   encrypt(mes,key) {
       this.mesCodes=[];
@@ -49,7 +31,6 @@ class VigenereCipheringMachine {
         counter++;
       } else counter=0
     }
-
     //////////////////
     let counter2=0;
     for(let i=0;i< this.mesCodes.length; i++){
@@ -60,19 +41,16 @@ class VigenereCipheringMachine {
         counter2++; //чтобы не учитывались пробелы , спецсимволы и цифры
       }
     }
-    console.log(this.encr.join(``))
-     if(this.direction=true){
+     if(this.direction==true){
       return this.encr.join(``);
      } else{
-      return this.encr.join(``).reverse();
+      return this.encr.reverse().join(``);
      }
-    
   }
   decrypt(mes,key) {
     if(!mes || !key){
       throw new Error(`Incorrect arguments!`)
     }
-    
     this.mesCodes=[];
     this.keyCodes=[];
     this.decr=[];
@@ -92,33 +70,39 @@ class VigenereCipheringMachine {
       counter++;
     } else counter=0
   }
-
   //////////////////
   let counter2=0;
-  for(let i=0;i< this.mesCodes.length; i++){
+  counter=0
+  for(let i in this.mesCodes){
     if(this.mesCodes[i]>26 ||this.mesCodes[i]<0 ){
-      this.encr[i]=String.fromCharCode(this.mesCodes[i]+65)
+      this.decr[i]=String.fromCharCode(this.mesCodes[i]+65)
     }else{
-      this.encr[i]=String.fromCharCode((this.mesCodes[i]-this.keyCodes[counter2])%26-65);
+      this.mesCodes[i]=this.mesCodes[i]>=this.keyCodes[counter2]?this.mesCodes[i]:this.mesCodes[i]+26; //Если номер буквы шифра больше номера буквы текста
+      this.decr[i]=String.fromCharCode((this.mesCodes[i]-this.keyCodes[counter2])%26+65);
       counter2++; //чтобы не учитывались пробелы , спецсимволы и цифры
     }
   }
-  console.log(this.encr.join(``))
-  return this.encr.join(``);
+  console.log(this.direction)
+  if(this.direction==true){
+    return this.decr.join(``);
+   } else{
+    return this.decr.reverse().join("");
+   }
   }
 }
 
-let test =new VigenereCipheringMachine(false)
 
-test.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey');
-test.encrypt('cryptography', 'verylongkeyword')
-//console.log(String.fromCharCode(33))
+//////Отладочный блок
+//let test =new VigenereCipheringMachine(true)
+
+//test.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey');
+//console.log(test.encrypt('cryptography', 'verylongkeyword'))
+//console.log(test.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
+//console.log(String.fromCharCode(82))
 //console.log(` `.charCodeAt(0))
-
+// X=(m+k)%26 y=(m-k)%26
+//A-65 Z-90
 module.exports = {
   VigenereCipheringMachine
 };
 
-// X=(m+k)%26 y=(m-k)%26
-
-//A-65 Z-90
