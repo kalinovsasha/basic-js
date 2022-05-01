fbb5e4a6373229322047d38fc9755ce932604dc1
Alexander <kalinovsasha@yandex.ru>
2022-05-01 14:12:15 +0300

    feat: add transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,42 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
- * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error(`'arr' parameter must be an instance of the Array!`)
+  }
+  let buf=[...arr]
+
+    for(let i in buf){
+      if(buf[i]=='--discard-prev'){
+        if(buf[Number(i)-1]){
+        buf.splice(i-1,2);
+        } else buf.splice(i,1);
+      }
+      if(buf[i]=='--double-prev'){
+        if(buf[Number(i)-1]){
+        buf.splice(i-1,2,buf[i-1],buf[i-1]);
+        } else buf.splice(i,1);
+      }
+      if(buf[i]=='--discard-next'){
+        if(buf[Number(i)+1]){
+        buf.splice(i,2);
+        } else buf.splice(i,1);
+      }
+      if(buf[i]=='--double-next'){
+        if(buf[Number(i)+1]){
+        buf.splice(i,2,buf[i+1],buf[i+1]);
+        } else buf.splice(i,1);
+      } 
+    } 
+  
+  return buf
+
 }
 
+//console.log(transform([`--double-prev`, 1, 2, 3]))
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]))
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5])),
 module.exports = {
   transform
 };
